6cd6e76af0dab5c38409caaf4338fad5305e07de
Kazuhiko Tsuchiya <kaz.tsuchiya@gmail.com>
2017-12-02 07:00:01 +0900

    additional files

---
+++ additional/mega_dropdown_menu.js
@@ -0,0 +1,5 @@
+// メガドロップダウンメニューで、展開中のメニューエリア内をクリックしたときに、ドロップダウンが閉じてしまう現象を解消する。
+
+$('.mega-dropdown-menu').click(function(e) {
+  e.stopPropagation();
+});
--- mega-dropdown-menu.js
+++
@@ -1,5 +0,0 @@
-// mega-dropdown-menu のメニューエリア内クリックでドロップダウンが閉じてしまう現象の解消
-
-$('.mega-dropdown-menu').click(function(e) {
-  e.stopPropagation();
-});
